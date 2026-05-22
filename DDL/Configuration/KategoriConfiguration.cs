45a3d6fd0b6cc09ddf35741b512585cd1b2fb7f2
yarencekemci <yarencekemci@gmail.com>
2026-05-22 19:57:10 +0300

    Kategori tablosu migration işlemleri 22.05.2026

--- DDL/Configuration/KategoriConfiguration.cs
+++ DDL/Configuration/KategoriConfiguration.cs
@@ -1,15 +1,17 @@
 ﻿using DDL.EntitiesTablolar;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DDL.Configuration
 {
-    internal class KategoriConfiguration
+    internal class KategoriConfiguration : IEntityTypeConfiguration<Kategoriler>
     {
-
+        public void Configure(EntityTypeBuilder<Kategoriler> builder)
+        {
+            builder.HasKey(x => x.Id); //PK
+            builder.Property(x => x.Id).UseIdentityColumn(); // SQL DE Increment, Seed ayarlarını 1,1 yapar.
+            builder.Property(x => x.Adi).IsRequired().HasMaxLength(250);
+        }
     }
 
 }
---
+++ DDL/Migrations/20260522165057_KategoriTabloEkle.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DDL.Migrations
+{
+    /// <inheritdoc />
+    public partial class KategoriTabloEkle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kategoriler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategoriler", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategoriler");
+        }
+    }
+}
